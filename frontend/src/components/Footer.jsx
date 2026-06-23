f331ce236bb7f8779de07af73de0943c074c597f
Vaishali-kale <vaishalikalekale@gmail.com>
2026-06-23 14:39:35 +0530

    Restore .gitignore and update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        Designed And Developed By CodeWithZeeshu
+        Designed And Developed By  Vaishali v kale
     </footer>
   )
 }
